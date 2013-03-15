fc0151f3f99f61f718c848e1f198f2266e2a8955
meegoo tsui <meegoo.tsui@gmail.com>
2013-03-15 16:10:09 +0800

    Modify env tool.

--- python/config_buildsys_env.py
+++ python/config_buildsys_env.py
@@ -12,16 +12,16 @@ import os, sys
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 from utils.printf   import printf
+from utils.env      import env
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## .bashrc文件路径。
-bashrc_file = os.path.expandvars("$HOME/.bashrc")
+env_file = os.path.expandvars("$HOME/.bashrc")
 ## 修改标志字符串。
-config_flag = "# Configure buildsys ENV"
+env_flag = "# Configure buildsys ENV"
 ## 添加内容。
-BUILD_SYS_PATH = os.path.expandvars("$BUILD_SYS_PATH")
-env_content = "#" + "+"*79 + "\n" + config_flag + "\n" + \
-"export BUILD_SYS_PATH="  + BUILD_SYS_PATH + \
+env_content = "\n#" + ">"*79 + "\n" + env_flag + "\n" + \
+"export BUILD_SYS_PATH="  + os.path.expandvars("$BUILD_SYS_PATH") + \
 '''
 if [ -f $BUILD_SYS_PATH/env/.env ] ; then
 	current_path=$PWD
@@ -29,28 +29,14 @@ if [ -f $BUILD_SYS_PATH/env/.env ] ; then
 	. .env
 	cd "$current_path"
 fi
-'''
+''' + \
+"#" + "<"*79 + "\n"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 主函数。
 def main():
 	printf.reset()
-
-	# judge config
-	fp = open(bashrc_file,"r")
-	for line in fp:
-		if config_flag in line:
-			fp.close()
-			printf.status("Config env for buildsys already.")
-			sys.exit(0)
-	fp.close()
-
-	# modify .bashrc
-	fp = open(bashrc_file,"a")
-	fp.write(env_content)
-	fp.close()
-	printf.status("Config env for buildsys success.")
-
+	env.modify(env_file, env_flag, env_content)
 	sys.exit(0)
 if __name__ == '__main__':
 	main()
---
+++ python/utils/env.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python
+#coding=utf-8 
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## @file    env.py
+#  @brief   环境变量设置功能。
+#  @author  meegoo.tsui@gmail.com
+#  @date    2013/03/15
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import os, sys, commands
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from   utils.printf   import printf
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+## 环境变量设置功能。
+class env:
+	## 得到主机IP
+	def hostip(self):
+		host_ip = ""
+		ips_get = "/sbin/ifconfig | grep -i \"inet addr:\" | awk {'print $2'} | sed -ne 's/addr://p'"
+		ips     = commands.getoutput(ips_get)
+		ipsList = ips.split("\n")
+		for i in ipsList:
+			if i.startswith("127."):
+				continue
+			else:
+				host_ip = i
+		printf.status("Host IP - " + host_ip)
+		return host_ip
+
+	## 修改env文件，添加内容到文件尾
+	def modify(self, filename, flag, content):
+		# judge
+		fp = open(filename, "r")
+		for line in fp:
+			if flag in line:
+				fp.close()
+				printf.status("With [" + flag + "] already.")
+				sys.exit(0)
+		fp.close()
+
+		# modify
+		fp = open(filename, "a")
+		fp.write(content)
+		fp.close()
+		printf.status("Write [" + flag + "] success.")
+
+		return
+
+## env对象
+env = env()
