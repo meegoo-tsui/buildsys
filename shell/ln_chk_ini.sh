31159af793258c19cf85c9c6bd90d57822c19a87
meegoo tsui <meegoo.tsui@gmail.com>
2013-03-22 14:09:38 +0800

    mdy.

--- shell/ln_chk_ini.sh
+++ shell/ln_chk_ini.sh
@@ -2,12 +2,7 @@
 
 cd $WORKSPACE
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-lncmd="ln -sf github/checkopen.git/github.ini github.ini"
-print_color.sh -g "$lncmd"
-$lncmd
-
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-lncmd="ln -sf local/gitosis-admin.git/local.ini local.ini"
+lncmd="ln -sf local/dm814x-dev.git/dm814x-dev.ini dm814x-dev.ini"
 print_color.sh -g "$lncmd"
 $lncmd
 
