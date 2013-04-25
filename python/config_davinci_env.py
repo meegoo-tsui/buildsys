27c01feb30340a378f9d791c502b4b13470ec8a6
meegoo tsui <meegoo.tsui@gmail.com>
2013-04-25 16:16:37 +0800

    modify env config

--- python/config_davinci_env.py
+++ python/config_davinci_env.py
@@ -37,6 +37,8 @@ env_content = \
   "export HOST_IP="    + env.hostip() + "\n" + \
   "export GATE_IP="    + '.'.join(str(x) for x in env.hostip().split(".")[:-1])  + ".1\n"  + \
   "export MASK_IP=255.255.255.0\n" + \
+  "\n# ARM GCC\n" + \
+  "export PATH=/opt/arm-2009q1-203-arm-none-linux-gnueabi/bin:$PATH\n" + \
   "\n# DM814x dev ENV\n" + \
   "export DM814X_DEV=" + env.home + "/workspace/local/dm814x/dm814x-dev.git\n" + \
   "export PATH=$DM814X_DEV/bin:$PATH\n" + \
