984e6f41b3c76fa290f75b809aee20c4bc7553f2
meegoo tsui <meegoo.tsui@gmail.com>
2013-05-27 19:18:43 +0800

    mdy backup tool

--- shell/davinci-out.sh
+++ shell/davinci-out.sh
@@ -8,6 +8,8 @@ help()
 	print_color.sh -g "  checkout and update local"
 	print_color.sh -g "$0 -s"
 	print_color.sh -g "  checkout and update mirror"
+	print_color.sh -g "$0 -d"
+	print_color.sh -g "  checkout and update mirror dev"
 	exit 1
 }
 
@@ -21,6 +23,8 @@ if [ "$1" == "-u" ]; then
 	check.py -f $WORKSPACE/local/gitosis-admin.git/davinci-all.ini -u
 elif [ "$1" == "-s" ]; then
 	check.py -f $WORKSPACE/local/gitosis-admin.git/davinci-all-mirror.ini -u
+elif [ "$1" == "-d" ]; then
+	check.py -f $WORKSPACE/local/gitosis-admin.git/davinci-dev-mirror.ini -u
 else
 	help
 fi
