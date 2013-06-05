b53506631030ba836e4fa6c1eb0ad6c7a41d503a
meegoo tsui <meegoo.tsui@gmail.com>
2013-06-05 16:14:21 +0800

    fixed bug.

--- shell/git-push-local.sh
+++ shell/git-push-local.sh
@@ -3,26 +3,26 @@
 # http://vxtindia.com/blog/how-to-migrate-github-repo-to-another-server-including-all-branches-and-tags/
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [ ! -d .git ]; then
-	print_color.sh -r "Error: not a git repos !"
+	print-color.sh -r "Error: not a git repos !"
 	exit 1
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [ ! -f .git/config.bak ]; then
 	cp .git/config .git/config.bak
-	print_color.sh -g "Backup the config."
+	print-color.sh -g "Backup the config."
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [ $# != 1 ]; then
-	print_color.sh -g "Usage:"
-	print_color.sh -g "$0 192.168.1.10:qt.git"
+	print-color.sh -g "Usage:"
+	print-color.sh -g "$0 192.168.1.10:qt.git"
 	exit 1
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 for remote in `git config -l | grep ^remote.local`; do
-	print_color.sh -g $remote
+	print-color.sh -g $remote
 done
 if [ "$remote" == "" ]; then
 	git remote add local git@$1
@@ -30,7 +30,7 @@ fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 for push in `git config -l | grep ^remote.local.push`; do
-	print_color.sh -g $push
+	print-color.sh -g $push
 done
 if [ "$push" == "" ]; then
 	git config --add remote.local.push '+refs/heads/*:refs/heads/*'
@@ -40,10 +40,10 @@ fi
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 for branch in `git branch -a --no-merged | grep remotes | grep -v HEAD | grep -v master`; do
 	for branch_new in `git branch -a --no-merged | grep -v remotes | grep -v HEAD | grep -v master | grep "${branch##*/}$"`; do
-		print_color.sh -g "branch: $branch_new esxist"
+		print-color.sh -g "branch: $branch_new esxist"
 	done
 	if [ "$branch_new" == "" ]; then
-		print_color.sh -g "branch: $branch_new track"
+		print-color.sh -g "branch: $branch_new track"
 		git branch --track ${branch##*/} $branch
 	fi
 done
