4e046d1d211114c56f5a6e4616b4c3a80d919f0d
meegoo tsui <meegoo.tsui@gmail.com>
2013-07-04 15:43:31 +0800

    eztool

---
+++ shell/git-tar-repos.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TIMESTAMP=`date +%Y-%m-%d.%H-%M-%S`
+GIT_PATH=/home/git
+REPOS_NAME=repositories
+PWD_PATH=$PWD
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "Backup local git repos, wait ..."
+if [ -d ${GIT_PATH}/${REPOS_NAME} ];then
+	cd ${GIT_PATH}/${REPOS_NAME}/..
+	cmd="sudo tar -jcf ${PWD_PATH}/${REPOS_NAME}.${TIMESTAMP}.tar.bz2 ${REPOS_NAME}"
+	print-color.sh -g "$cmd"
+	exec $cmd
+	cmd="sudo chown ${USER}:${USER} ${PWD_PATH}/${REPOS_NAME}.${TIMESTAMP}.tar.bz2"
+	print-color.sh -g "$cmd"
+	exec $cmd
+	cd -
+else
+	print-color.sh -r "Error path: ${GIT_PATH}/${REPOS_NAME}"
+	exit 1
+fi
+exit 0
+
