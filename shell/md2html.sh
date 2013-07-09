8124655daea40bb9249e950f0829f271c0ae87f0
meegoo tsui <meegoo.tsui@gmail.com>
2013-07-09 13:55:35 +0800

    fix bug

--- shell/md2html.sh
+++ shell/md2html.sh
@@ -2,22 +2,22 @@
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 if [ $# != 1 ]; then
-	print_color.sh -g "Usage:"
-	print_color.sh -g "$0 file"
+	print-color.sh -g "Usage:"
+	print-color.sh -g "$0 file"
 	exit 1
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 md="$1.md"
 if [ ! -f $md ] ; then
-	print_color.sh -r "No file - $md"
+	print-color.sh -r "No file - $md"
 	exit 1
 fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 html="$1.html"
 cmd="pandoc -s -S $md -o $html"
-print_color.sh -g "$cmd"
+print-color.sh -g "$cmd"
 $cmd
 exit 0
 
