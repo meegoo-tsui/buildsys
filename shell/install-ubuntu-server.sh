2f809d4dc49745ac86d749eb5342b3664372c2d8
meegoo tsui <meegoo.tsui@gmail.com>
2013-09-22 15:10:38 +0800

    eztool for ubuntu server install

---
+++ shell/install-ubuntu-server.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+
+reset
+print-color.sh -g "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+print-color.sh -g "Install software as a ubuntu server ..."
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "update and upgrade ..."
+sudo apt-get update
+sudo apt-get upgrade
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "server: ssh"
+sudo apt-get -y install openssh-server
+sudo /etc/init.d/ssh restart
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "server: samba"
+sudo apt-get -y install samba samba-common samba-common-bin system-config-samba
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "server: tftp"
+sudo apt-get -y install tftpd tftp xinetd
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "server: nfs"
+sudo apt-get -y install nfs-kernel-server
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -g "server: svn and git"
+sudo apt-get -y install apache2 subversion libapache2-svn python-subversion
+sudo apt-get -y install openssh-server openssh-client git python-setuptools
+
+print-color.sh -g "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+print-color.sh -g "Install done."
+
