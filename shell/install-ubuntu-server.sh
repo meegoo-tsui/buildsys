b4f7b8b908db4761ba14becf9896008080a35cb1
meegoo tsui <meegoo.tsui@gmail.com>
2013-09-23 14:55:04 +0800

    svn server install

--- shell/install-ubuntu-server.sh
+++ shell/install-ubuntu-server.sh
@@ -28,7 +28,7 @@ sudo apt-get -y install nfs-kernel-server
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print-color.sh -g "server: svn and git"
-sudo apt-get -y install apache2 subversion libapache2-svn python-subversion
+sudo apt-get -y install apache2 subversion libapache2-svn python-subversion libapache2-mod-wsgi
 sudo apt-get -y install openssh-server openssh-client git python-setuptools gitweb
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
