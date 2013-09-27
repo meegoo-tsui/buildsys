a8a2737eb196e0db2875411e11cb01e6ecc3cfde
meegoo tsui <meegoo.tsui@gmail.com>
2013-09-27 09:07:38 +0800

    save before clean.

--- shell/clean.sh
+++ shell/clean.sh
@@ -4,10 +4,16 @@ echo "clear ..."
 set -e
 
 if [ $# != 1 ]; then
+	CMD="patch.py -f build.ini -a 2"
+	echo $CMD
+	$CMD
 	CMD="patch.py -f build.ini -a 1"
 	echo $CMD
 	$CMD	
 else
+	CMD="patch.py -f build.ini -a 2 -o $1"
+	echo $CMD
+	$CMD
 	CMD="patch.py -f build.ini -a 1 -o $1"
 	echo $CMD
 	$CMD
