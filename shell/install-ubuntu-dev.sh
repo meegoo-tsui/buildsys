419c554a514a7b08be69d04edaf6bd0d947f2a87
meegoo tsui <meegoo.tsui@gmail.com>
2013-10-11 18:09:11 +0800

    ad.

--- shell/install-ubuntu-dev.sh
+++ shell/install-ubuntu-dev.sh
@@ -5,19 +5,27 @@ print-color.sh -g "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print-color.sh -g "Install software as a ubuntu dev host ..."
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "update and upgrade ..."
-sudo apt-get update
-sudo apt-get upgrade
+exec_cmd()
+{
+	print-color.sh -g "$1"
+	$1
+}
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "install dev tools ..."
-sudo apt-get -y install u-boot-tools bison flex colordiff
+if [ $# != 0 ]; then
+	exec_cmd "sudo apt-get update"
+	exec_cmd "sudo apt-get upgrade"
+fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "config ..."
-sudo ln -sf /bin/bash /bin/sh
-sudo chmod +s /bin/chmod
-sudo chmod +s /bin/mknod
+print-color.sh -y "install dev tools ..."
+exec_cmd "sudo apt-get -y install u-boot-tools bison flex colordiff"
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -y "config ..."
+exec_cmd "sudo ln -sf /bin/bash /bin/sh"
+exec_cmd "sudo chmod +s /bin/chmod"
+exec_cmd "sudo chmod +s /bin/mknod"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print-color.sh -g "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
--- shell/install-ubuntu-server.sh
+++ shell/install-ubuntu-server.sh
@@ -5,35 +5,43 @@ print-color.sh -g "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print-color.sh -g "Install software as a ubuntu server ..."
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "update and upgrade ..."
-sudo apt-get update
-sudo apt-get upgrade
+exec_cmd()
+{
+	print-color.sh -g "$1"
+	$1
+}
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "server: ssh"
-sudo apt-get -y install openssh-server
-sudo /etc/init.d/ssh restart
+if [ $# != 0 ]; then
+	exec_cmd "sudo apt-get update"
+	exec_cmd "sudo apt-get upgrade"
+fi
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "server: samba"
-sudo apt-get -y install samba samba-common samba-common-bin system-config-samba
+print-color.sh -y "server: ssh"
+exec_cmd "sudo apt-get -y install openssh-server"
+exec_cmd "sudo /etc/init.d/ssh restart"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "server: tftp"
-sudo apt-get -y install tftpd tftp xinetd
+print-color.sh -y "server: samba"
+exec_cmd "sudo apt-get -y install samba samba-common samba-common-bin system-config-samba"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "server: nfs"
-sudo apt-get -y install nfs-kernel-server
+print-color.sh -y "server: tftp"
+exec_cmd "sudo apt-get -y install tftpd tftp xinetd"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "server: svn and git"
-sudo apt-get -y install apache2 subversion libapache2-svn python-subversion libapache2-mod-wsgi
-sudo apt-get -y install openssh-server openssh-client git python-setuptools gitweb
+print-color.sh -y "server: nfs"
+exec_cmd "sudo apt-get -y install nfs-kernel-server"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print-color.sh -g "usefull tools"
-sudo apt-get -y install vim gnome-schedule
+print-color.sh -y "server: svn and git"
+exec_cmd "sudo apt-get -y install apache2 subversion libapache2-svn python-subversion libapache2-mod-wsgi"
+exec_cmd "sudo apt-get -y install openssh-server openssh-client git python-setuptools gitweb highlight"
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print-color.sh -y "usefull tools"
+exec_cmd "sudo apt-get -y install vim gnome-schedule"
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 print-color.sh -g "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
