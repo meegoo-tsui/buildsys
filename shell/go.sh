a545572e08146c8b775b93697aa2640dd20c488d
meegoo <meegoo.tsui@gmail.com>
2013-10-23 18:16:00 +0800

    ad

--- shell/go.sh
+++ shell/go.sh
@@ -36,11 +36,13 @@ function do_save()
 	fi
 }
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-function do_clean()
+function do_clean() # save then clean
 {
 	if [ $# != 1 ]; then
+		exec_cmd "patch.py -f $ini -a 2"
 		exec_cmd "patch.py -f $ini -a 1"	
 	else
+		exec_cmd "patch.py -f $ini -a 2 -o $1"
 		exec_cmd "patch.py -f $ini -a 1 -o $1"
 	fi
 }
